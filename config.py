406ae606262fa7e592b855e78e2d5c92bdc912fe
Matthew Lanier <xorobabel@gmail.com>
2021-08-11 18:02:43 -0500

    Update env variable

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['POSTGRESQL_DATABASE_URL']
